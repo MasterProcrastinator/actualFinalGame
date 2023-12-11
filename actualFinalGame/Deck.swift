79aeec7f7812b80f8d73b6dc6cbf79193d466581
Alvin Chen <alvin.chen25@d155.org>
2023-12-11 09:53:10 -0600

    save properly please

--- actualFinalGame/Deck.swift
+++ actualFinalGame/Deck.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+
+enum Suit: CaseIterable{
+    case spade
+    case club
+    case diamond
+    case heart
+    
+ 
+    
+}
+
+enum CardValue: CaseIterable{
+    case ace
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+    
+    var value: Int {
+        switch self{
+        case.ace:
+            return 1
+        case.two:
+            return 2
+        case.three:
+            return 3
+        case.four:
+            return 4
+        case.five:
+            return 5
+        case.six:
+            return 6
+        case.seven:
+            return 7
+        case.eight:
+            return 8
+        case.nine:
+            return 9
+        case.ten, .jack, .king, .queen:
+            return 10
+            
+        }
+        
+    }
+}
+
+struct Card {
+    let suit: Suit
+    let worth: CardValue
+    
+    
+}
+
+func createDeck() -> [Card]{
+    var deck: [Card] = []
+    for s in Suit.allCases{
+        for c in CardValue.allCases{
+            deck.append(.init(suit: s, worth: c))
+        }
+    }
+    return deck
+}
+
+
+func createImage(name theCard :Card) -> String{
+    
+    var string: String
+    string = "\(theCard.suit)" + "\(theCard.worth)"
+    
+    return string
+}
+
--- actualFinalGame/ViewController.swift
+++ actualFinalGame/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageOutlet1: UIImageView!
+
+    @IBOutlet weak var imageOutlet2: UIImageView!
+    
+    var deck = createDeck()
+    var dealer: [Card] = []
+    var player: [Card] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        deck.shuffle()
+    
+        dealer.append(deck.removeLast())
+        dealer.append(deck.removeLast())
+        
+        player.append(deck.removeLast())
+        
+        debugPrint(createImage(name: deck.removeLast()))
+
     }
 
+    @IBAction func hitAction(_ sender: UIButton) {
+        player.append(deck.removeLast())
+    }
 
+    
 }
 
