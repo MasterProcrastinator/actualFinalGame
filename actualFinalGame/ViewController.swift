a5f386f865971be9504d8cfaf09d9b7377a0e6c4
Alvin Chen <alvin.chen25@d155.org>
2023-12-17 22:35:41 -0600

    main game finished

--- actualFinalGame/ViewController.swift
+++ actualFinalGame/ViewController.swift
@@ -28,6 +28,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerImageOutlet4: UIImageView!
     
     @IBOutlet weak var playerImageOutlet5: UIImageView!
+    
+    let bustAlert = UIAlertController(title: "Bust", message: "You've gone over 21 and lost", preferredStyle: .alert) //done
+    
+    let dealerBustAlert = UIAlertController(title: "Dealer has Busted", message: "You've Won!", preferredStyle: .alert) //done
+    
+    let victoryAlert = UIAlertController(title: "Congratulations", message: "Your hand won", preferredStyle: .alert) //done
+
+    let lossAlert = UIAlertController(title: "Better luck next time", message: "the dealer won", preferredStyle: .alert) //done
+    
+    let twentyOneAlert = UIAlertController(title: "hit 21!", message: "instant victory for you", preferredStyle: .alert) //done
+    
+    let twentyOneDealerAlert = UIAlertController(title: "Dealer hit 21", message: "say goodbye to your money", preferredStyle: .alert)
+    
+    let tieAlert = UIAlertController(title: "Tie", message: "bets are returned", preferredStyle: .alert)//done
+    
+
+    
     var test = 0
     var handValue = 0
     var dealerValue = 0
@@ -36,8 +53,10 @@ class ViewController: UIViewController {
     var dealer: [Card] = []
     var player: [Card] = []
     var count = 1
-    var isBust = false
-    var dealerIsBust = false
+
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -59,9 +78,6 @@ class ViewController: UIViewController {
             return
         }
         
-        if (isBust) {
-            return
-        }
 
         player.append(deck.removeLast())
         
@@ -97,12 +113,108 @@ class ViewController: UIViewController {
                 }
             }
         }
+        
         handValue = countCards
 
         if(countCards > 21) {
-            isBust = true
+
+            let replayAction = UIAlertAction(title: "Play Again", style: .default){ (action) in
+                self.test = 0
+                self.handValue = 0
+                self.dealerValue = 0
+                self.dealerCount = 0
+                self.deck = createDeck()
+                self.dealer = []
+                self.player = []
+                self.count = 1
+                
+                self.deck.shuffle()
+            
+                self.dealer.append(self.deck.removeLast())
+                self.imageOutlet1.image = UIImage(named: "\(createImage(name: self.dealer[0]))")
+                
+                self.dealer.append(self.deck.removeLast())
+                
+                self.player.append(self.deck.removeLast())
+                
+                self.playerImageOutlet1.image = UIImage(named: "\(createImage(name: self.player[0]))")
+                
+                self.playerImageOutlet2.image = UIImage(named: "")
+                
+                self.playerImageOutlet3.image = UIImage(named: "")
+                
+                self.playerImageOutlet4.image = UIImage(named: "")
+                
+                self.playerImageOutlet5.image = UIImage(named: "")
+                
+                self.imageOutlet2.image = UIImage(named: "card_back")
+            
+                self.imageOutlet3.image = UIImage(named: "")
+                
+                self.imageOutlet4.image = UIImage(named: "")
+                
+                self.imageOutlet5.image = UIImage(named: "")
+                
+                
+
+            }
+
+            
+            bustAlert.addAction(replayAction)
+
+            present(bustAlert, animated: true, completion: nil)
+            
+            
         }
 
+        if(countCards == 21){
+            let replayAction = UIAlertAction(title: "Play Again", style: .default){ (action) in
+                self.test = 0
+                self.handValue = 0
+                self.dealerValue = 0
+                self.dealerCount = 0
+                self.deck = createDeck()
+                self.dealer = []
+                self.player = []
+                self.count = 1
+                
+                self.deck.shuffle()
+            
+                self.dealer.append(self.deck.removeLast())
+                self.imageOutlet1.image = UIImage(named: "\(createImage(name: self.dealer[0]))")
+                
+                self.dealer.append(self.deck.removeLast())
+                
+                self.player.append(self.deck.removeLast())
+                
+                self.playerImageOutlet1.image = UIImage(named: "\(createImage(name: self.player[0]))")
+                
+                self.playerImageOutlet2.image = UIImage(named: "")
+                
+                self.playerImageOutlet3.image = UIImage(named: "")
+                
+                self.playerImageOutlet4.image = UIImage(named: "")
+                
+                self.playerImageOutlet5.image = UIImage(named: "")
+                
+                self.imageOutlet2.image = UIImage(named: "card_back")
+            
+                self.imageOutlet3.image = UIImage(named: "")
+                
+                self.imageOutlet4.image = UIImage(named: "")
+                
+                self.imageOutlet5.image = UIImage(named: "")
+                
+                
+
+            }
+
+            
+            twentyOneAlert.addAction(replayAction)
+
+            present(twentyOneAlert, animated: true, completion: nil)
+        }
+        
         print("\(countCards)")
         
     }
@@ -128,11 +240,106 @@ class ViewController: UIViewController {
                 }
             }
         }
-        dealerValue = countCards
+
 
         if(countCards > 21) {
-            dealerIsBust = true
+            
+            let replayAction = UIAlertAction(title: "Play Again", style: .default){ (action) in
+                self.test = 0
+                self.handValue = 0
+                self.dealerValue = 0
+                self.dealerCount = 0
+                self.deck = createDeck()
+                self.dealer = []
+                self.player = []
+                self.count = 1
+                
+                self.deck.shuffle()
+            
+                self.dealer.append(self.deck.removeLast())
+                self.imageOutlet1.image = UIImage(named: "\(createImage(name: self.dealer[0]))")
+                
+                self.dealer.append(self.deck.removeLast())
+                
+                self.player.append(self.deck.removeLast())
+                
+                self.playerImageOutlet1.image = UIImage(named: "\(createImage(name: self.player[0]))")
+                
+                self.playerImageOutlet2.image = UIImage(named: "")
+                
+                self.playerImageOutlet3.image = UIImage(named: "")
+                
+                self.playerImageOutlet4.image = UIImage(named: "")
+                
+                self.playerImageOutlet5.image = UIImage(named: "")
+                
+                self.imageOutlet2.image = UIImage(named: "card_back")
+            
+                self.imageOutlet3.image = UIImage(named: "")
+                
+                self.imageOutlet4.image = UIImage(named: "")
+                
+                self.imageOutlet5.image = UIImage(named: "")
+                
+                
+
+            }
+            
+            if(countCards == 21){
+                let replayAction = UIAlertAction(title: "Play Again", style: .default){ (action) in
+                    self.test = 0
+                    self.handValue = 0
+                    self.dealerValue = 0
+                    self.dealerCount = 0
+                    self.deck = createDeck()
+                    self.dealer = []
+                    self.player = []
+                    self.count = 1
+                    
+                    self.deck.shuffle()
+                
+                    self.dealer.append(self.deck.removeLast())
+                    self.imageOutlet1.image = UIImage(named: "\(createImage(name: self.dealer[0]))")
+                    
+                    self.dealer.append(self.deck.removeLast())
+                    
+                    self.player.append(self.deck.removeLast())
+                    
+                    self.playerImageOutlet1.image = UIImage(named: "\(createImage(name: self.player[0]))")
+                    
+                    self.playerImageOutlet2.image = UIImage(named: "")
+                    
+                    self.playerImageOutlet3.image = UIImage(named: "")
+                    
+                    self.playerImageOutlet4.image = UIImage(named: "")
+                    
+                    self.playerImageOutlet5.image = UIImage(named: "")
+                    
+                    self.imageOutlet2.image = UIImage(named: "card_back")
+                
+                    self.imageOutlet3.image = UIImage(named: "")
+                    
+                    self.imageOutlet4.image = UIImage(named: "")
+                    
+                    self.imageOutlet5.image = UIImage(named: "")
+                    
+                    
+
+                }
+
+                
+                twentyOneDealerAlert.addAction(replayAction)
+
+                present(twentyOneDealerAlert, animated: true, completion: nil)
+            }
+
+            
+            dealerBustAlert.addAction(replayAction)
+
+            present(dealerBustAlert, animated: true, completion: nil)
         }
+        
+        dealerValue = countCards
         
         while(dealerValue < 16 && dealer.count != 5){
             dealer.append(deck.removeLast())
@@ -150,6 +357,150 @@ class ViewController: UIViewController {
         }
         if(dealerCount >= 4){
             imageOutlet5.image = UIImage(named: "\(createImage(name: dealer[4]))")
+        }
+        
+        if(dealerValue > handValue){
+            let replayAction = UIAlertAction(title: "Play Again", style: .default){ (action) in
+                self.test = 0
+                self.handValue = 0
+                self.dealerValue = 0
+                self.dealerCount = 0
+                self.deck = createDeck()
+                self.dealer = []
+                self.player = []
+                self.count = 1
+                
+                self.deck.shuffle()
+            
+                self.dealer.append(self.deck.removeLast())
+                self.imageOutlet1.image = UIImage(named: "\(createImage(name: self.dealer[0]))")
+                
+                self.dealer.append(self.deck.removeLast())
+                
+                self.player.append(self.deck.removeLast())
+                
+                self.playerImageOutlet1.image = UIImage(named: "\(createImage(name: self.player[0]))")
+                
+                self.playerImageOutlet2.image = UIImage(named: "")
+                
+                self.playerImageOutlet3.image = UIImage(named: "")
+                
+                self.playerImageOutlet4.image = UIImage(named: "")
+                
+                self.playerImageOutlet5.image = UIImage(named: "")
+                
+                self.imageOutlet2.image = UIImage(named: "card_back")
+            
+                self.imageOutlet3.image = UIImage(named: "")
+                
+                self.imageOutlet4.image = UIImage(named: "")
+                
+                self.imageOutlet5.image = UIImage(named: "")
+                
+                
+
+            }
+
+            
+            lossAlert.addAction(replayAction)
+
+            present(lossAlert, animated: true, completion: nil)
+        }
+        
+        else if(handValue > dealerValue){
+            let replayAction = UIAlertAction(title: "Play Again", style: .default){ (action) in
+                self.test = 0
+                self.handValue = 0
+                self.dealerValue = 0
+                self.dealerCount = 0
+                self.deck = createDeck()
+                self.dealer = []
+                self.player = []
+                self.count = 1
+                
+                self.deck.shuffle()
+            
+                self.dealer.append(self.deck.removeLast())
+                self.imageOutlet1.image = UIImage(named: "\(createImage(name: self.dealer[0]))")
+                
+                self.dealer.append(self.deck.removeLast())
+                
+                self.player.append(self.deck.removeLast())
+                
+                self.playerImageOutlet1.image = UIImage(named: "\(createImage(name: self.player[0]))")
+                
+                self.playerImageOutlet2.image = UIImage(named: "")
+                
+                self.playerImageOutlet3.image = UIImage(named: "")
+                
+                self.playerImageOutlet4.image = UIImage(named: "")
+                
+                self.playerImageOutlet5.image = UIImage(named: "")
+                
+                self.imageOutlet2.image = UIImage(named: "card_back")
+            
+                self.imageOutlet3.image = UIImage(named: "")
+                
+                self.imageOutlet4.image = UIImage(named: "")
+                
+                self.imageOutlet5.image = UIImage(named: "")
+                
+                
+
+            }
+
+            
+            victoryAlert.addAction(replayAction)
+
+            present(victoryAlert, animated: true, completion: nil)
+        }
+        
+        else{
+            let replayAction = UIAlertAction(title: "Play Again", style: .default){ (action) in
+                self.test = 0
+                self.handValue = 0
+                self.dealerValue = 0
+                self.dealerCount = 0
+                self.deck = createDeck()
+                self.dealer = []
+                self.player = []
+                self.count = 1
+                
+                self.deck.shuffle()
+            
+                self.dealer.append(self.deck.removeLast())
+                self.imageOutlet1.image = UIImage(named: "\(createImage(name: self.dealer[0]))")
+                
+                self.dealer.append(self.deck.removeLast())
+                
+                self.player.append(self.deck.removeLast())
+                
+                self.playerImageOutlet1.image = UIImage(named: "\(createImage(name: self.player[0]))")
+                
+                self.playerImageOutlet2.image = UIImage(named: "")
+                
+                self.playerImageOutlet3.image = UIImage(named: "")
+                
+                self.playerImageOutlet4.image = UIImage(named: "")
+                
+                self.playerImageOutlet5.image = UIImage(named: "")
+                
+                self.imageOutlet2.image = UIImage(named: "card_back")
+            
+                self.imageOutlet3.image = UIImage(named: "")
+                
+                self.imageOutlet4.image = UIImage(named: "")
+                
+                self.imageOutlet5.image = UIImage(named: "")
+                
+                
+
+            }
+
+            
+            tieAlert.addAction(replayAction)
+
+            present(tieAlert, animated: true, completion: nil)
         }
         
     }
